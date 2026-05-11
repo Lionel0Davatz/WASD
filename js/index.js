ead0d4865bb646344b0ba89f7deb790bbfef342b
lda <lionel.davatz@clavisit.com>
2026-05-11 20:03:30 +0200

    Movement Rework

--- js/index.js
+++ js/index.js
@@ -1,44 +1,58 @@
 import { walls } from "./createMap.js";
 
 const player = document.getElementById("player");
+let x = 0;
 let xDirection = 0;
+
+let y = 0;
 let yDirection = 0;
+
+const keys = {};
+
+let playerSpeed = 3
 
 function boxCollision(p, o) {
   const player = p.getBoundingClientRect();
   const obstacle = o.getBoundingClientRect();
 
-  if (player.x + player.width >= obstacle.x &&
+  if (
+    player.x + player.width >= obstacle.x &&
     player.x <= obstacle.x + obstacle.width &&
     player.y + player.height >= obstacle.y &&
     player.y <= obstacle.y + obstacle.height
   ) {
-    return console.log("hat berührt")
+    console.log("hat berührt");
   }
 }
 
 document.addEventListener("keydown", (event) => {
-  if (event.key === "w") {
-    yDirection -= 5;
-  }
-
-  if (event.key === "s") {
-    yDirection += 5;
-  }
-
-  if (event.key === "d") {
-    xDirection += 5;
-  }
-
-  if (event.key === "a") {
-    xDirection -= 5;
-  }
-
-  player.style.transform = `translate(${xDirection}px, ${yDirection}px)`;
-
-  walls.forEach((wall) => {
-    boxCollision(player, wall)
-  })
+  keys[event.code] = true
 });
 
+document.addEventListener("keyup", (event) => {
+  keys[event.code] = false
+});
 
+function movePlayer() {
+  yDirection = 0;
+  xDirection = 0;
+
+  if(keys["KeyW"]) yDirection = -playerSpeed;
+  if(keys["KeyS"]) yDirection = playerSpeed;
+  if(keys["KeyD"]) xDirection = playerSpeed;
+  if(keys["KeyA"]) xDirection = -playerSpeed;
+
+  x += xDirection;
+  y += yDirection;
+
+
+  player.style.transform = `translate(${x}px, ${y}px)`;
+
+  walls.forEach((wall) => {
+    boxCollision(player, wall);
+  });
+
+  requestAnimationFrame(movePlayer);
+}
+
+movePlayer();
